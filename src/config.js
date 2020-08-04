93b696687b3c4ab9c88db9165d9e01fcf337178f
Taeil Kwak <taeil2@gmail.com>
2020-08-04 10:43:11 -0500

    began scaffolding

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  // fill or replace these later
+  API_ENDPOINT: '',
+  TOKEN_KEY: 'myclientele-jwt-secret',
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'autoprefixer';
+import 'normalize.css';
+import './index.scss';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './contexts/UserContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
