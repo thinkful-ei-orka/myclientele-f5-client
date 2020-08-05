08597227fa9bcdfa4e503a7459252ae00c79b0a4
Taeil Kwak <taeil2@gmail.com>
2020-08-05 12:40:54 -0500

    set up the client api service

---
+++ src/services/client-api-service.js
@@ -0,0 +1,46 @@
+import TokenService from './token-service';
+import config from '../config';
+
+const ClientApiService = {
+  getAllClients() {
+    return fetch(`${config.API_ENDPOINT}/clients`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+    .then(res =>
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    )
+  },
+  getClient(client_id) {
+    return fetch(`${config.API_ENDPOINT}/clients/${client_id}`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+    .then(res =>
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    )
+  },
+  addClient(client) {
+    return fetch(`${config.API_ENDPOINT}/clients`, {
+      method: 'POST',
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(client)
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
+}
+
+export default ClientApiService;
