0b88c33a51f8afa7558d55351acb86310eb5194e
Jonathan Clark <jonclark822@gmail.com>
2020-08-07 08:02:35 -0700

    finalized client card styling

--- src/components/ScheduleView/ScheduleDropDown/ScheduleDropDown.js
+++ src/components/ScheduleView/ScheduleDropDown/ScheduleDropDown.js
@@ -1,21 +1,5 @@
 import React from 'react';
-const arrow = (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    className='icon icon-tabler icon-tabler-direction'
-    width='24'
-    height='24'
-    viewBox='0 0 24 24'
-    strokeWidth='1.5'
-    stroke='#2c3e50'
-    fill='none'
-    strokeLinecap='round'
-    strokeLinejoin='round'>
-    <path stroke='none' d='M0 0h24v24H0z' />
-    <path d='M9 10l3 -3l3 3' />
-    <path d='M9 14l3 3l3 -3' />
-  </svg>
-);
+import arrow from '../../../images/arrow.svg';
 
 class ScheduleDropDown extends React.Component {
   constructor(props) {
@@ -56,7 +40,9 @@ class ScheduleDropDown extends React.Component {
     return (
       <div className='schedule-drop-down'>
         <div className='dropdown-title' onClick={() => this.toggleList()}>
-          {this.state.dropdownTitle} {arrow}
+          {' '}
+          {this.state.dropdownTitle}
+          <img src={arrow} alt='drop down arrow' />
         </div>
         {this.state.listOpen ? this.handleListOpen() : ''}
       </div>
