142d9f93f40f13fb40a0bdca16f9914b5de76cc1
Taeil Kwak <taeil2@gmail.com>
2020-08-10 10:46:07 -0500

    just deployed to production

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export default {
   // fill or replace these later
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'myclientele-jwt-secret',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://myclientele-f5-api.herokuapp.com/api',
+  TOKEN_KEY: process.env.TOKEN_KEY,
+  GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
 }
