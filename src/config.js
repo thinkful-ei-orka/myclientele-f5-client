6c9a0a8f9a3bdcfcf74c563d1809b8e7b9f334af
Taeil Kwak <taeil2@gmail.com>
2020-08-10 12:59:29 -0500

    updated the config file

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 export default {
-  // fill or replace these later
   // API_ENDPOINT: 'http://localhost:8000/api',
   API_ENDPOINT: 'https://myclientele-f5-api.herokuapp.com/api',
-  // TOKEN_KEY: 'myclientele-jwt-secret',
-  // GOOGLE_API_KEY: 'AIzaSyALTeDJY0y4Ui6Q8wtOE0hZooVKsPTapt0',
-
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
   GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
 }
