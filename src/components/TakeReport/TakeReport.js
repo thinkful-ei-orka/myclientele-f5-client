fc35229c615b52090919a2a399f1b9d4d71c14c8
JZPoteat <jzpoteat@gmail.com>
2020-08-12 12:34:29 -0400

    working on image upload.  Post works, but not getting the image

--- src/components/TakeReport/TakeReport.js
+++ src/components/TakeReport/TakeReport.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReportsApiService from '../../services/reports-api-service';
-import { withRouter, Link } from 'react-router-dom';
-import './takereport.scss';
-import S3ApiService from '../../services/s3-api-service';
+import React from "react";
+import ReportsApiService from "../../services/reports-api-service";
+import { withRouter, Link } from "react-router-dom";
+import "./takereport.scss";
+import S3ApiService from "../../services/s3-api-service";
 
 class TakeReport extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       reports: [],
+      signedUrl: "",
+      file_name: "",
+      img_src: null,
       isLoading: true,
     };
   }
@@ -18,12 +21,26 @@ class TakeReport extends React.Component {
 
   onFormSubmit = (e) => {
     e.preventDefault();
-    const notes = e.target['report-text-input'].value;
-    const photo_url = e.target['report-photo-input'].value;
+    const notes = e.target["report-text-input"].value;
+    const photoInput = e.target["report-photo-input"];
+    const file = photoInput.files[0];
+    console.log('name ', file.name, '... type ',file.type);
+    S3ApiService.getUploadUrl(file.name, file.type).then((res) => {
+      console.log('response url', res);
+      return fetch(res.url, {
+        method: "PUT",
+        body: file
+      })
+    })
+      .then((data) => this.setState({
+        img_src: data.url
+      }))
+      .catch((error) => console.log(error))
+    
 
-    ReportsApiService.addReport(this.client_id, notes, photo_url).then(() => {
-      this.props.history.push('/schedule');
-    });
+    // ReportsApiService.addReport(this.client_id, notes, photo_url).then(() => {
+    //   this.props.history.push('/schedule');
+    // });
   };
   componentDidMount() {
     if (this.state.reports.length === 0) {
@@ -32,7 +49,6 @@ class TakeReport extends React.Component {
         this.setState({ reports: res, isLoading: false });
       });
     }
-    S3ApiService.getUploadUrl().then((res) => console.log(res));
   }
 
   render() {
@@ -40,54 +56,61 @@ class TakeReport extends React.Component {
       return <div>Loading...</div>;
     }
     return (
-      <div className='take-a-report'>
+      <div className="take-a-report">
         <h1>Take a Report</h1>
-        <div className='schedule-card'>
-          <div className='company-logo'>
-            <img src='https://via.placeholder.com/150' alt={this.data.name} />
+        <div className="schedule-card">
+          <div className="company-logo">
+            <img src="https://via.placeholder.com/150" alt={this.data.name} />
           </div>
-          <div className='information-area'>
+          <div className="information-area">
             <h2>{this.data.name} </h2>
-            <div className='location'>{this.data.location}</div>
+            <div className="location">{this.data.location}</div>
           </div>
         </div>
-        <div className='report-input'>
+        <div className="report-input">
           <form
-            className='take-a-report-form'
-            onSubmit={(e) => this.onFormSubmit(e)}>
-            <label htmlFor='report-text-input'>Write your report:</label>
+            className="take-a-report-form"
+            onSubmit={(e) => this.onFormSubmit(e)}
+          >
+            <label htmlFor="report-text-input">Write your report:</label>
             <textarea
-              type='text'
-              id='report-text-input'
-              name='report-text-input'></textarea>
-            <label htmlFor='report-photo-input'>Add a photo:</label>
+              type="text"
+              id="report-text-input"
+              name="report-text-input"
+            ></textarea>
+            <label htmlFor="report-photo-input">Add a photo:</label>
             <input
-              type='url'
-              name='report-photo-input'
-              id='report-photo-input'
-              placeholder='https://example.com'
-              pattern='https://.*'
-              size='30'
-              required></input>
-            <button className='btn'>Submit</button>
+              type="file"
+              accept="image/*"
+              name="report-photo-input"
+              id="report-photo-input"
+              alt="alt_text"
+              required
+            ></input>
+            <button className="btn">Submit</button>
           </form>
         </div>
-        <section aria-label='Your reports' className='report-list'>
+        {this.state.img_src 
+        ? <img src={this.state.img_src}alt='img' />
+        : ""
+        }
+        <section aria-label="Your reports" className="report-list">
           <h1>Previous Reports</h1>
 
-          <ul className='report-list-ul'>
+          <ul className="report-list-ul">
             {this.state.reports.map((report) => (
               <Link
                 key={report.id}
                 to={`/reports/${report.id}`}
-                className='reportList-link'>
-                <li className='report-li' id={report.id}>
+                className="reportList-link"
+              >
+                <li className="report-li" id={report.id}>
                   <img
-                    className='company-logo'
+                    className="company-logo"
                     src={report.photo_url}
                     alt={report.name}
                   />
-                  <p className='information-area'>{report.notes}</p>
+                  <p className="information-area">{report.notes}</p>
                 </li>
               </Link>
             ))}
--- src/services/s3-api-service.js
+++ src/services/s3-api-service.js
@@ -1,8 +1,8 @@
 import config from '../config';
 
 const S3ApiService = {
-  getUploadUrl() {
-    return fetch(`${config.API_ENDPOINT}/presignedurl`).then((res) =>
+  getUploadUrl(name, type) {
+    return fetch(`${config.API_ENDPOINT}/presignedurl?name=${name}&type=${type}`).then((res) =>
       !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
     );
   },
