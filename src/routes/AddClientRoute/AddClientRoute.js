294f209bd9ca0051a67572a0fee88c3ea367442f
Sherry McCarty <sherrydmccarty@gmail.com>
2020-08-14 12:37:35 -0400

    initial commit

--- src/routes/AddClientRoute/AddClientRoute.js
+++ src/routes/AddClientRoute/AddClientRoute.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import GoogleMap from '../../components/GoogleMap/GoogleMap';
+// import GoogleMap from '../../components/GoogleMap/GoogleMap';
+import GoogleExperiment from '../../components/GoogleExperiment/GoogleExperiment'
 
 export default function AddClientRoute() {
   return <section>
-    <GoogleMap />
+    <GoogleExperiment />
   </section>
 }
+
+
