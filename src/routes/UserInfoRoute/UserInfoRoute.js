2c7b50c5e686ca2026cdb154a9e2e22f574286a5
M Jones <jonesman2800@gmail.com>
2020-08-14 21:10:30 -0400

    minor user route styling and user info service build

--- src/routes/UserInfoRoute/UserInfoRoute.js
+++ src/routes/UserInfoRoute/UserInfoRoute.js
@@ -1,29 +1,76 @@
 import React from 'react';
 import PrivateContext from '../../contexts/PrivateContext';
 import './userinforoute.scss';
+import TokenService from '../../services/token-service';
+import config from '../../config'
 
 class UserInfo extends React.Component {
     static contextType = PrivateContext;
     constructor(props) {
         super(props);
         this.state = {
-            editMode: false,
-            saving: false,
+            viewMode: true,
+            saveComplete: false,
         };
     }
 
+    shouldComponentUpdate() {
+        // if (this.state.viewMode && this.state.saveComplete) {
+        //     return true
+        // }
+        return this.state.viewMode === false && this.state.saveComplete === true
+    };
+
     handleEdit = () => {
-        this.setState({editMode: !this.state.editMode})
-        console.log('Editing...')
+        this.setState({viewMode: !this.state.viewMode})
+        console.log(this.state.viewMode)
     };
 
-    handleSave = () => {
+    componentDidMount = () => {
         console.log('Saving...')
-        this.setState({saving: !this.state.saving})
-        //sav
-        this.setState({saving: !this.state.saving})
+        // this.setState({saving: !this.state.saving})
+        return fetch(`${config.API_ENDPOINT}/users/contact`, {
+            headers: {
+                authorization: `bearer ${TokenService.getAuthToken()}`,
+            }
+        })
+        .then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json())
+        .then((data) => {
+            console.log('response here:',data)
+        });
+        // this.setState({saveComplete: true})
     };
 
+    renderNameField = (currentName) => {
+        return <input type='text' name='input-name' placeholder={currentName} />
+    }
+    renderUsernameField = (currentUsername) => {
+        return (
+            <input type='text' name='input-username' placeholder={currentUsername} />
+        )
+    }
+    renderPasswordField = () => {
+        return (
+            <fieldset>
+                <input type='password' name='input-password'/>
+                <input type='password' name='input-repassword' placeholder='Re-enter Password' />
+            </fieldset>
+        )
+    }
+
+    renderEmailField = (currentEmail) => {
+        return (
+            <input type='text' name='input-email' placeholder={currentEmail} />
+        )
+    }
+
+    renderPhonNumberField = (currentPhoneNumber) => {
+        return (
+            <input type='text' name='input-phone' placeholder={currentPhoneNumber} />
+        )
+    }
+    
     render() {
         const userAccountInfo = this.context.user
         const userContactInfo = this.context.userContact
@@ -32,24 +79,26 @@ class UserInfo extends React.Component {
         return (
             <div className='account-page'>
                 <div className='account-title'>My Account</div>
-                <div className='save-delete-buttons'>
-                    <button className='edit-button btn' onClick={() => this.handleEdit()}>{this.state.editMode ? 'Cancel' : 'Edit'}</button>
-                    <button className='save-button btn' onClick={() => this.handleSave()}>Save</button>
-                </div>
-                <div className='account-area'>
-                    <div className='account-name'>Name</div>
-                    <div className='account-name-field'>{this.state.editMode ? null : userAccountInfo.name}</div>
-                    <div className='account-username'>Username</div>
-                    <div className='account-username-field'>{this.state.editMode ? null : userAccountInfo.username}</div>
-                    <div className='account-password'>Password</div>
-                    <div className='account-password-field'>{this.state.editMode ? null : null}</div>
-                    <div className='account-email'>Email</div>
-                    <div className='account-email-field'>{this.state.editMode ? null : null }</div>
-                    <div className='account-phone'>Phone Number</div>
-                    <div className='account-phone-field'>{this.state.editMode ? null : null }</div>
-                    <div className='account-companyid'>Company Code</div>
-                    <div className='account-companyid-field'>{this.state.editMode ? null : this.context.user.company_id}</div>
-                </div>
+                <form className='update-account-form' onSubmit={() => this.handleSave()}>
+                    <div className='save-edit-buttons'>
+                        <button className='edit-button btn' onClick={() => this.handleEdit()}>{this.state.viewMode ? 'Edit': 'Cancel'}</button>
+                        <button type='submit' className='save-button btn'>Save</button>
+                    </div>
+                    <div className='account-area'>
+                        <label htmlFor="input-name" className='account-name'>Name</label>
+                        <div className='account-name-field'>{this.state.viewMode ?  userAccountInfo.name: this.renderNameField(userAccountInfo.name)}</div>
+                        <label htmlFor="input-username" className='account-username'>Username</label>
+                        <div className='account-username-field'>{this.state.viewMode ? userAccountInfo.username : null}</div>
+                        <label htmlFor="input-password" className='account-password'>Password</label>
+                        <div className='account-password-field'>{this.state.viewMode ? null : this.renderPasswordField()}</div>
+                        <label htmlFor="input-email" className='account-email'>Email</label>
+                        <div className='account-email-field'>{this.state.viewMode ? null : this.renderEmailField('email') }</div>
+                        <label htmlFor="name" className='account-phone'>Phone Number</label>
+                        <div htmlFor="input-phone" className='account-phone-field'>{this.state.viewMode ? null : this.renderNameField(userAccountInfo.name)}</div>
+                        <div className='account-companyid'>Company Code</div>
+                        <div className='account-companyid-field'>{this.state.viewMode ? this.context.user.company_id : null}</div>
+                    </div>
+                </form>
             </div>
             
             
---
+++ src/services/user-api-service.js
@@ -0,0 +1,21 @@
+import TokenService from "./token-service";
+import config from "../config";
+
+const UserApiService = {
+
+    getUserContactInfo() {
+        return fetch(`${config.API_ENDPOINT}/users/contact`, {
+            headers: {
+                authorization: `bearer ${TokenService.getAuthToken()}`,
+            },
+        })
+        .then((res) =>
+            !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json())
+
+        },
+
+    
+
+}
+
+export default UserApiService
