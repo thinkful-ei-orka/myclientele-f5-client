c5452c44856cd32093e7360ddb4830db4eb3a403
Jonathan Clark <jonclark822@gmail.com>
2020-08-20 10:25:56 -0700

    switched to a sidebar menu package

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import logo from '../../images/logo.png';
 import './Header.scss';
+import './hamburger.scss';
 
 import Modal from 'react-modal';
 import { withRouter, Link } from 'react-router-dom';
 import LoginForm from '../LoginForm/LoginForm';
 import SignUpForm from '../SignUpForm/SignUpForm';
 import UserContext from '../../contexts/UserContext';
+import hamburger from '../../images/hamburger.svg';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+import { slide as Menu } from 'react-burger-menu';
 
 class Header extends React.Component {
   state = {
     loginIsOpen: false,
     signUpIsOpen: false,
+    menuOpen: false,
   };
+  showSettings(event) {
+    event.preventDefault();
+  }
 
   static contextType = UserContext;
 
@@ -45,24 +53,29 @@ class Header extends React.Component {
           <div className='logo'>
             <img src={logo} alt='my clientele logo'></img>
           </div>
-          <div className='header-buttons'>
-            <button className='btn' onClick={(e) => this.setLoginOpen(true)}>
-              Login
-            </button>
-            <Modal
-              isOpen={this.state.loginIsOpen}
-              onRequestClose={(e) => this.setLoginOpen(false)}>
-              <LoginForm closeModal={(e) => this.setLoginOpen(false)} />
-            </Modal>
-            <button className='btn' onClick={(e) => this.setSignUpOpen(true)}>
-              Sign Up
-            </button>
-            <Modal
-              isOpen={this.state.signUpIsOpen}
-              onRequestClose={(e) => this.setSignUpOpen(false)}>
-              <SignUpForm closeModal={(e) => this.setSignUpOpen(false)} />
-            </Modal>
+
+          <div className='main-menu'>
+            <Menu right>
+              <button className='btn' onClick={(e) => this.setLoginOpen(true)}>
+                Login
+              </button>
+
+              <button className='btn' onClick={(e) => this.setSignUpOpen(true)}>
+                Sign Up
+              </button>
+            </Menu>
           </div>
+
+          <Modal
+            isOpen={this.state.loginIsOpen}
+            onRequestClose={(e) => this.setLoginOpen(false)}>
+            <LoginForm closeModal={(e) => this.setLoginOpen(false)} />
+          </Modal>
+          <Modal
+            isOpen={this.state.signUpIsOpen}
+            onRequestClose={(e) => this.setSignUpOpen(false)}>
+            <SignUpForm closeModal={(e) => this.setSignUpOpen(false)} />
+          </Modal>
         </header>
       );
     }
@@ -74,21 +87,24 @@ class Header extends React.Component {
             <img src={logo} alt='my clientele logo'></img>
           </Link>
         </div>
-        <div className='header-buttons'>
-          <Link to='/add-client'>
-            <button className='add-client btn'>Add Client</button>
-          </Link>
-          <Link to='/reports'>
-            <button className='reports btn'>Reports</button>
-          </Link>
-          <Link to='/my-account'>
-            <button className='user-info btn'>My Account</button>
-          </Link>
-          <button
-            className='logout-button btn'
-            onClick={() => this.handleLogOut()}>
-            Logout
-          </button>
+
+        <div className='main-menu'>
+          <Menu right isOpen={this.state.menuOpen}>
+            <Link to='/add-client'>
+              <button className='add-client btn'>Add Client</button>
+            </Link>
+            <Link to='/reports'>
+              <button className='reports btn'>Reports</button>
+            </Link>
+            <Link to='/my-account'>
+              <button className='user-info btn'>My Account</button>
+            </Link>
+            <button
+              className='logout-button btn'
+              onClick={() => this.handleLogOut()}>
+              Logout
+            </button>
+          </Menu>
         </div>
       </header>
     );
