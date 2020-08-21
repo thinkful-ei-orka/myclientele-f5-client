2fc5d919f2b94ca3d3c32d6daed2cbd68e54620c
M Jones <jonesman2800@gmail.com>
2020-08-21 12:12:12 -0400

    Editable fields for My Client Route and Copy of company id

--- src/routes/MyAccountRoute/MyAccountRoute.js
+++ src/routes/MyAccountRoute/MyAccountRoute.js
@@ -3,7 +3,7 @@ import PrivateContext from '../../contexts/PrivateContext';
 import './MyAccountRoute.scss';
 import TokenService from '../../services/token-service';
 import config from '../../config';
-import {CopyToClipboard, contextType} from 'react-copy-to-clipboard';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import UserApiService from '../../services/user-api-service';
 import { info } from 'autoprefixer';
 
@@ -18,7 +18,7 @@ class MyAccountRoute extends React.Component {
             rePassword:'',
             phoneNumber: '',
             email: '',
-            error:''
+            error:'',
         };
     }
 
@@ -39,10 +39,7 @@ class MyAccountRoute extends React.Component {
         console.log(infoToUpdate)
 
         UserApiService.updateUserContactInfo(infoToUpdate)
-        .catch((res) => {
-            this.setState({ error: res.error})
-        })
-        .then(
+        .then((res) =>
             this.setState({
                 name: '',
                 username: '',
@@ -51,7 +48,11 @@ class MyAccountRoute extends React.Component {
                 phoneNumber: '',
                 email: '',
                 error:''})
-        );
+                // error: res.message,})
+        )
+        .catch((res) => {
+            this.setState({ error: res.error})
+        })
     };
 
     componentDidMount = () => {
@@ -98,23 +99,23 @@ class MyAccountRoute extends React.Component {
                     <h2 id="title">My Account</h2>
                     <label htmlFor="input-name" className='account-name'>Name</label>
                     <input type='text' name='input-name'
-                    value={userContactInfo.name}
+                    defaultValue={userContactInfo.name}
                     placeholder='Enter new name' onChange={this.setName}/>
                     <label htmlFor="input-username" className='account-username'>Username</label>
-                    <input type='text' name='input-username' value={userContactInfo.username} placeholder='Enter new username' onChange={this.setUsername}/>
+                    <input type='text' name='input-username' defaultValue={userContactInfo.username} placeholder='Enter new username' onChange={this.setUsername}/>
                     <label htmlFor="input-password" className='account-password'>Password</label>
                     <fieldset>
                         <input type='password' name='input-password'placeholder='Enter new password'onChange={this.setPassword}/>
                         <input type='password' name='input-repassword' placeholder='Re-enter password' onChange={this.setRepassword}/>
                     </fieldset>
                     <label htmlFor="input-email" className='account-email'>Email</label>
-                    <input type='text' name='input-email' value={userContactInfo.email} placeholder='Enter new email address' onChange={this.setEmail}/>
+                    <input type='text' name='input-email' defaultValue={userContactInfo.email} placeholder='Enter new email address' onChange={this.setEmail}/>
                     <label htmlFor="name" className='account-phone'>Phone Number</label>
-                    <input type='text' name='input-phone' value={userContactInfo.phone_number} placeholder='Enter new phone number' onChange={this.setPhoneNumber}/>
+                    <input type='text' name='input-phone' defaultValue={userContactInfo.phone_number} placeholder='Enter new phone number' onChange={this.setPhoneNumber}/>
                     <label>Company Code</label>
-                    <input className="company-code" type="text" value={this.context.user.company_id} disabled></input>
+                    <input className="company-code" type="text" value={this.context.user.company_id} readOnly></input>
                     <CopyToClipboard text={this.context.user.company_id}>
-                        <button className="btn">Copy</button>
+                        <button className="btn" type="button">Copy</button>
                     </CopyToClipboard>
                     <button type='submit' className='save-button btn'>Save</button>
                 </form>
