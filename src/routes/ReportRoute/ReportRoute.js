0c90ee7b2adf66afd92385287051dc0ebd896da2
Jonathan Clark <jonclark822@gmail.com>
2020-08-26 10:02:21 -0700

    finished report edit styling

--- src/routes/ReportRoute/ReportRoute.js
+++ src/routes/ReportRoute/ReportRoute.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ReportsApiService from "../../services/reports-api-service";
-import PrivateContext from "../../contexts/PrivateContext";
-import "./reportroute.scss";
-import ClientApiService from "../../services/client-api-service";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ReportsApiService from '../../services/reports-api-service';
+import PrivateContext from '../../contexts/PrivateContext';
+import './reportroute.scss';
+import ClientApiService from '../../services/client-api-service';
 // import Swiper core and required components
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
-import "swiper/swiper.scss";
-import "swiper/components/navigation/navigation.scss";
-import "swiper/components/pagination/pagination.scss";
-import "swiper/components/scrollbar/scrollbar.scss";
-import UserApiService from "../../services/user-api-service";
-import TakeReport from "../../components/TakeReport/TakeReport";
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+import UserApiService from '../../services/user-api-service';
+import TakeReport from '../../components/TakeReport/TakeReport';
 // install Swiper components
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 class ReportRoute extends React.Component {
@@ -25,9 +25,9 @@ class ReportRoute extends React.Component {
       confirmingDelete: false,
       enteringPassword: false,
       editing: false,
-      password: "",
+      password: '',
       client: [],
-      data: null
+      data: null,
     };
   }
 
@@ -41,7 +41,7 @@ class ReportRoute extends React.Component {
             <img
               src={photo}
               alt={index}
-              className="slide_photo"
+              className='slide_photo'
               key={`slide ${index}`}
             />
           </SwiperSlide>
@@ -54,13 +54,16 @@ class ReportRoute extends React.Component {
           navigation
           scrollbar={{ draggable: true }}
           onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log("slide change")}
-        >
+          onSlideChange={() => console.log('slide change')}>
           {slides}
         </Swiper>
       );
     } else {
-      return <p id="no_photos_statement">You don't have any photos for this report</p>
+      return (
+        <p id='no_photos_statement'>
+          You don't have any photos for this report
+        </p>
+      );
     }
   };
 
@@ -104,30 +107,33 @@ class ReportRoute extends React.Component {
 
   renderConfirmingDelete = () => {
     return (
-      <div id="confirm_delete_box">
+      <div id='confirm_delete_box'>
         {this.state.enteringPassword ? (
-          <form id="confirm_delete_form" onSubmit={this.deleteReport}>
+          <form id='confirm_delete_form' onSubmit={this.deleteReport}>
             <p>
               Please enter your password to confirm you are deleting this report
             </p>
-            <label htmlFor="password">Password: </label>
+            <label htmlFor='password'>Password: </label>
             {this.state.error && <p>{this.state.error}. Please try again</p>}
             <input
-              type="password"
-              name="password"
-              id="confirm_delete_password_input"
+              type='password'
+              name='password'
+              id='confirm_delete_password_input'
               value={this.state.password}
               onChange={this.setPassword}
             />
-            <section id="delete_box_buttons">
-              <button type="submit" id="confirm_delete_report_button">
+            <section id='delete_box_buttons'>
+              <button
+                className='btn'
+                type='submit'
+                id='confirm_delete_report_button'>
                 Delete
               </button>
               <button
-                type="button"
-                id="cancel_delete_report_button"
-                onClick={this.setConfirmDelete}
-              >
+                type='button'
+                className='btn'
+                id='cancel_delete_report_button'
+                onClick={this.setConfirmDelete}>
                 Cancel
               </button>
             </section>
@@ -137,17 +143,20 @@ class ReportRoute extends React.Component {
             <h3>Are you sure you want to delete this report?</h3>
             <br />
             <p>
-              You will lose all of your notes and photos for this report.{" "}
+              You will lose all of your notes and photos for this report.{' '}
               <strong>This cannot be undone!</strong>
             </p>
-            <section id="delete_box_buttons">
+            <section id='delete_box_buttons'>
               <button
-                id="confirm_delete_report"
-                onClick={this.SetEnteringPassword}
-              >
+                className='btn'
+                id='confirm_delete_report'
+                onClick={this.SetEnteringPassword}>
                 Confirm
               </button>
-              <button id="cancel_delete_report" onClick={this.setConfirmDelete}>
+              <button
+                className='btn outline'
+                id='cancel_delete_report'
+                onClick={this.setConfirmDelete}>
                 Cancel
               </button>
             </section>
@@ -165,7 +174,7 @@ class ReportRoute extends React.Component {
     UserApiService.confirmPassword(password)
       .then(() => ReportsApiService.deleteReport(this.state.report.id))
       .then(() => {
-        this.props.history.push("/reports");
+        this.props.history.push('/reports');
         window.location.reload();
       })
       .catch((res) => {
@@ -188,36 +197,39 @@ class ReportRoute extends React.Component {
     return (
       <>
         {this.state.editing ? (
-          <TakeReport data={this.state.report.id}/>
+          <TakeReport data={this.state.report.id} />
         ) : (
           <>
-            <section className="single-report-section">
+            <section className='single-report-section'>
               {this.state.confirmingDelete && this.renderConfirmingDelete()}
 
-              <p id="report-date">
+              <p id='report-date'>
                 <b>Report Date: </b>
                 {`${newDate.toLocaleDateString()} @ ${newDate.toLocaleTimeString()}`}
               </p>
-              <section className="report_header">
+              <section className='report_header'>
                 <button
-                  id="delete_report_button"
-                  onClick={this.setConfirmDelete}
-                >
-                  <i className="fas fa-trash"></i>
+                  className='btn outline'
+                  id='delete_report_button'
+                  onClick={this.setConfirmDelete}>
+                  <i className='fas fa-trash'></i>
                 </button>
                 <h2>{this.state.client.name}</h2>
-                <button id="edit_report_button" onClick={this.setEditing}>
-                  <i className="far fa-edit"></i>
+                <button
+                  className='btn'
+                  id='edit_report_button'
+                  onClick={this.setEditing}>
+                  <i className='far fa-edit'></i>
                 </button>
               </section>
               <h3>Notes:</h3>
-              <div className="notes-area">{this.state.report.notes}</div>
+              <div className='notes-area'>{this.state.report.notes}</div>
               <h3>Photos: </h3>
             </section>
             {Object.keys(this.state.report).length !== 0
               ? this.renderSwiper()
-              : ""}
-            <div className="zoom_container"></div>
+              : ''}
+            <div className='zoom_container'></div>
           </>
         )}
       </>
