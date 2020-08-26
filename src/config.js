d7cb0267cbcdd3748c7e4351fcc1f279a2070bd1
Jonathan Clark <jonclark822@gmail.com>
2020-08-26 10:21:52 -0700

    changed api-endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  // API_ENDPOINT: 'https://myclientele-f5-api.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://myclientele-f5-api.herokuapp.com/api',
   TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
   GOOGLE_API_KEY: process.env.REACT_APP_GOOGLE_API_KEY,
 };
